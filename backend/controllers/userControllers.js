bdcc242dd16f01a9f59ae2431bfcea059d3ebfd2
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-11-13 15:32:24 +0100

    :BUILD: ==> cryptojs

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,17 @@
+
+/**  ---- JS DOCUMENTATION USERCONTROLLERS.JS ----
+ * 
+
+ * 
+ */
+
+
+
+/* ##########   MES DECLARATIONS   ################ */
+const bcrypt = require('bcrypt');
+const jwt    = require('jsonwebtoken');
+const crypto = require('crypto-js');
+
+require('dotenv')
+    .config({ path: './config/.env' }); 
+/* ################################################ */
