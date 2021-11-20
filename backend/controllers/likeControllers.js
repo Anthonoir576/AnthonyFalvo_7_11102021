b25e0c648e37444970b68d4b051af7db9acf2c57
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-11-20 16:27:19 +0100

    :FEAT: ==> LIKE CONTROLER controle ok

--- backend/controllers/likeControllers.js
+++ backend/controllers/likeControllers.js
@@ -27,19 +27,61 @@ exports.likePost = (request, response, next) => {
     const postId       = parseInt(request.params.postId);
 
     if (postId <= 0) {
-        return response.status(400).json({ 'error': 'paramètre invalide' });
+        return response.status(400).json({ 'message': 'paramètre invalide' });
     };
 
-    models.Post.findOne({
-        where: { id : postId }
-    }).then( (post) => {
+    if (userId) {
+        models.Post.findOne({
+            where: { id : postId }
+        }).then( (post) => {
+    
+            if (post) {
+                models.Like.findOne({
+                    where : { userId: userId, postId: postId }
+                }).then((tableauLike) => {
+    
+                    if (!tableauLike) {
+                        models.Like.create({
+                            userId: userId,
+                            postId: postId,
+                            like: 1,
+                            dislike: 0
+                        }).then(() => {
+                            post.update({
+                                likes: post.likes + 1
+                            }).then(() => response.status(200).json({ 'message': 'Le like est ajouté à la publication [code:01]' }))
+                              .catch(() => response.status(400).json({ 'message': 'Le like n\'est pas ajouté à la publication [code:01]'}));
+                        })
+                        .catch(() => response.status(400).json({ 'message': 'Le like ne fonctionne pas [code:01]'})); 
+                        
+                        
+                    } else if (tableauLike && tableauLike.like == 1) {
+                        tableauLike.destroy().then(() => {
+                            post.update({
+                                likes: post.likes - 1
+                            }).then(() => response.status(200).json({ 'message': 'Le like est retiré de la publication [code:02]' }))
+                              .catch(() => response.status(400).json({ 'message': 'Le like n\'est pas retiré de la publication [code:02]'}));
+                        })
+                        .catch(() => response.status(400).json({ 'message': 'Le retrait du like ne fonctionne pas [code:02]'})); 
+                        
+                    // On peut plus etre a zero vu que ta destroy si 0 sur les deux    
+                    } else if (tableauLike && tableauLike.dislike == 0 && tableauLike.like == 0) {
+                        tableauLike.update({
+                            like: 1
+                        }).then(() => {
+                            post.update({
+                                likes: post.likes + 1
+                            }).then(() => response.status(200).json({ 'message': 'Le like est ajouté à la publication [code:03]' }))
+                              .catch(() => response.status(400).json({ 'message': 'Le like n\'est pas ajouté la publication [code:03]'}));
+                        })
+                        .catch(() => response.status(400).json({ 'message': 'Le like ne fonctionne pas [code:03]'}));  
+                          
+    
+                    } else if (tableauLike && tableauLike.dislike != 0) {
+                        return response.status(400).json({ 'message': 'Vous avez déjà disliker cette publication [code:04]' });
+                    };
 
-        if (post) {
-            models.Like.findOne({
-                where : { userId: userId, postId: postId }
-            }).then((tableauLike) => {
-
-                if (!tableauLike) {
+                }).catch(() => {
                     models.Like.create({
                         userId: userId,
                         postId: postId,
@@ -48,60 +90,20 @@ exports.likePost = (request, response, next) => {
                     }).then(() => {
                         post.update({
                             likes: post.likes + 1
-                        }).then(() => response.status(200).json({ 'message': 'Le like est ajouté à la publication [code:01]' }))
-                          .catch(() => response.status(400).json({ 'message': 'Le like n\'est pas ajouté à la publication [code:01]'}));
+                        }).then(() => response.status(200).json({ 'message': 'Le like est ajouté à la publication [code:05]' }))
+                          .catch(() => response.status(400).json({ 'message': 'Le like n\'est pas ajouté à la publication [code:05]'}));
                     })
-                    .catch(() => response.status(400).json({ 'message': 'Le like ne fonctionne pas [code:01]'})); 
-                    
-                    
-                } else if (tableauLike && tableauLike.like == 1) {
-                    tableauLike.destroy().then(() => {
-                        post.update({
-                            likes: post.likes - 1
-                        }).then(() => response.status(200).json({ 'message': 'Le like est retiré de la publication [code:02]' }))
-                          .catch(() => response.status(400).json({ 'message': 'Le like n\'est pas retiré de la publication [code:02]'}));
-                    })
-                    .catch(() => response.status(400).json({ 'message': 'Le retrait du like ne fonctionne pas [code:02]'})); 
-                    
-                // On peut plus etre a zero vu que ta destroy si 0 sur les deux    
-                } else if (tableauLike && tableauLike.dislike == 0 && tableauLike.like == 0) {
-                    tableauLike.update({
-                        like: 1
-                    }).then(() => {
-                        post.update({
-                            likes: post.likes + 1
-                        }).then(() => response.status(200).json({ 'message': 'Le like est ajouté à la publication [code:03]' }))
-                          .catch(() => response.status(400).json({ 'message': 'Le like n\'est pas ajouté la publication [code:03]'}));
-                    })
-                    .catch(() => response.status(400).json({ 'message': 'Le like ne fonctionne pas [code:03]'}));  
-                      
-
-                } else if (tableauLike && tableauLike.dislike != 0) {
-                    return response.status(400).json({ 'message': 'Vous avez déjà disliker cette publication [code:04]' });
-                };
-
-
-            }).catch(() => {
-                models.Like.create({
-                    userId: userId,
-                    postId: postId,
-                    like: 1,
-                    dislike: 0
-                }).then(() => {
-                    post.update({
-                        likes: post.likes + 1
-                    }).then(() => response.status(200).json({ 'message': 'Le like est ajouté à la publication [code:05]' }))
-                      .catch(() => response.status(400).json({ 'message': 'Le like n\'est pas ajouté à la publication [code:05]'}));
-                })
-                  .catch(() => response.status(400).json({ 'message': 'Le like ne fonctionne pas [code:05]'}));
-            });
-        } else {
-            return response.status(404).json({ 'error': 'Aucune publication trouvé ! [code:06]' })
-        };
-    }).catch(() => response.status(500).json({ 'error': 'Erreur serveur ! [code:07]' }));
+                      .catch(() => response.status(400).json({ 'message': 'Le like ne fonctionne pas [code:05]'}));
+                });
+            } else {
+                return response.status(404).json({ 'message': 'Aucune publication trouvé ! [code:06]' });
+            };
+        }).catch(() => response.status(500).json({ 'message': 'Erreur serveur ! [code:07]' }));
+    } else {
+        return response.status(401).json({ 'message': 'Vous n\'êtes pas authentifié !' });
+    };
 
 };
-
 exports.dislikePost = (request, response, next) => {
 
     const token        = request.cookies.jwt;
@@ -110,19 +112,61 @@ exports.dislikePost = (request, response, next) => {
     const postId       = parseInt(request.params.postId);
 
     if (postId <= 0) {
-        return response.status(400).json({ 'error': 'paramètre invalide' });
+        return response.status(400).json({ 'message': 'paramètre invalide' });
     };
 
-    models.Post.findOne({
-        where: { id : postId }
-    }).then( (post) => {
-
-        if (post) {
-            models.Like.findOne({
-                where : { userId: userId, postId: postId }
-            }).then((tableauLike) => {
-
-                if (!tableauLike) {
+    if (userId) {
+        models.Post.findOne({
+            where: { id : postId }
+        }).then( (post) => {
+    
+            if (post) {
+                models.Like.findOne({
+                    where : { userId: userId, postId: postId }
+                }).then((tableauLike) => {
+    
+                    if (!tableauLike) {
+                        models.Like.create({
+                            userId: userId,
+                            postId: postId,
+                            like: 0,
+                            dislike: 1
+                        }).then(() => {
+                            post.update({
+                                dislikes: post.dislikes + 1
+                            }).then(() => response.status(200).json({ 'message': 'Le dislike est ajouté à la publication [code:01]' }))
+                              .catch(() => response.status(400).json({ 'message': 'Le dislike n\'est pas ajouté à la publication [code:01]'}));
+                        })
+                        .catch(() => response.status(400).json({ 'message': 'Le dislike ne fonctionne pas [code:01]'})); 
+                        
+    
+                    } else if (tableauLike && tableauLike.dislike == 1) {
+                        tableauLike.destroy().then(() => {
+                            post.update({
+                                dislikes: post.dislikes - 1
+                            }).then(() => response.status(200).json({ 'message': 'Le dislike est retiré de la publication [code:02]' }))
+                              .catch(() => response.status(400).json({ 'message': 'Le dislike n\'est pas retiré de la publication [code:02]'}));
+                        })
+                        .catch(() => response.status(400).json({ 'message': 'Le retrait du dislike ne fonctionne pas [code:02]'})); 
+                        
+                    // On peut plus etre a zero vu que ta destroy si 0 sur les deux      
+                    } else if (tableauLike && tableauLike.dislike == 0 && tableauLike.like == 0) {
+                        tableauLike.update({
+                            dislike: 1
+                        }).then(() => {
+                            post.update({
+                                dislikes: post.dislikes + 1
+                            }).then(() => response.status(200).json({ 'message': 'Le dislike est ajouté à la publication [code:03]' }))
+                              .catch(() => response.status(400).json({ 'message': 'Le dislike n\'est pas ajouté la publication [code:03]'}));
+                        })
+                        .catch(() => response.status(400).json({ 'message': 'Le dislike ne fonctionne pas [code:03]'}));  
+                          
+    
+                    } else if (tableauLike && tableauLike.like != 0) {
+                        return response.status(400).json({ 'message': 'Vous avez déjà liker cette publication [code:04]' });
+                    };
+    
+                }).catch(() => {
                     models.Like.create({
                         userId: userId,
                         postId: postId,
@@ -131,57 +175,17 @@ exports.dislikePost = (request, response, next) => {
                     }).then(() => {
                         post.update({
                             dislikes: post.dislikes + 1
-                        }).then(() => response.status(200).json({ 'message': 'Le dislike est ajouté à la publication [code:01]' }))
-                          .catch(() => response.status(400).json({ 'message': 'Le dislike n\'est pas ajouté à la publication [code:01]'}));
+                        }).then(() => response.status(200).json({ 'message': 'Le dislike est ajouté à la publication [code:05]' }))
+                          .catch(() => response.status(400).json({ 'message': 'Le dislike n\'est pas ajouté à la publication [code:05]'}));
                     })
-                    .catch(() => response.status(400).json({ 'message': 'Le dislike ne fonctionne pas [code:01]'})); 
-                    
-
-                } else if (tableauLike && tableauLike.dislike == 1) {
-                    tableauLike.destroy().then(() => {
-                        post.update({
-                            dislikes: post.dislikes - 1
-                        }).then(() => response.status(200).json({ 'message': 'Le dislike est retiré de la publication [code:02]' }))
-                          .catch(() => response.status(400).json({ 'message': 'Le dislike n\'est pas retiré de la publication [code:02]'}));
-                    })
-                    .catch(() => response.status(400).json({ 'message': 'Le retrait du dislike ne fonctionne pas [code:02]'})); 
-                    
-                // On peut plus etre a zero vu que ta destroy si 0 sur les deux      
-                } else if (tableauLike && tableauLike.dislike == 0 && tableauLike.like == 0) {
-                    tableauLike.update({
-                        dislike: 1
-                    }).then(() => {
-                        post.update({
-                            dislikes: post.dislikes + 1
-                        }).then(() => response.status(200).json({ 'message': 'Le dislike est ajouté à la publication [code:03]' }))
-                          .catch(() => response.status(400).json({ 'message': 'Le dislike n\'est pas ajouté la publication [code:03]'}));
-                    })
-                    .catch(() => response.status(400).json({ 'message': 'Le dislike ne fonctionne pas [code:03]'}));  
-                      
-
-                } else if (tableauLike && tableauLike.like != 0) {
-                    return response.status(400).json({ 'message': 'Vous avez déjà liker cette publication [code:04]' });
-                };
-
-
-            }).catch(() => {
-                models.Like.create({
-                    userId: userId,
-                    postId: postId,
-                    like: 0,
-                    dislike: 1
-                }).then(() => {
-                    post.update({
-                        dislikes: post.dislikes + 1
-                    }).then(() => response.status(200).json({ 'message': 'Le dislike est ajouté à la publication [code:05]' }))
-                      .catch(() => response.status(400).json({ 'message': 'Le dislike n\'est pas ajouté à la publication [code:05]'}));
-                })
-                  .catch(() => response.status(400).json({ 'message': 'Le dislike ne fonctionne pas [code:05]'}));
-            });
-        } else {
-            return response.status(404).json({ 'error': 'Aucune publication trouvé ! [code:06]' })
-        };
-    }).catch(() => response.status(500).json({ 'error': 'Erreur serveur ! [code:07]' }));
-
+                      .catch(() => response.status(400).json({ 'message': 'Le dislike ne fonctionne pas [code:05]'}));
+                });
+            } else {
+                return response.status(404).json({ 'message': 'Aucune publication trouvé ! [code:06]' })
+            };
+        }).catch(() => response.status(500).json({ 'message': 'Erreur serveur ! [code:07]' }));    
+    } else {
+        return response.status(401).json({ 'message': 'Vous n\'êtes pas authentifié !' });
+    };
 };
 /* ################################################ */
