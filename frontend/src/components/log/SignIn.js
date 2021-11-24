ea853d7f0f4d6dc6eabd48ebc48b3ff2690f56dc
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-11-24 11:13:35 +0100

    :FEAT: ajout .env pour facilité le changement dadresse api + premiere requete en cour

--- frontend/src/components/log/SignIn.js
+++ frontend/src/components/log/SignIn.js
@@ -1,14 +1,34 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-
+ 
 const SignIn = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const formLogin = (e) => {
+        e.preventDefault();
 
+        const emailOrPasswordError = document.querySelector('.error');
+
+        axios({
+            method: "POST",
+            url: `${process.env.REACT_APP_API_URL}api/auth/login`,
+            withCredentials: true,
+            data: {
+                email: email,
+                password: password
+            }
+        }).then((result) => {
+            if (result.data.errors){
+                emailOrPasswordError.innerHTML = (result.data.errors)
+            } else {
+                window.location = '/';
+            }
+        }).catch((errors) => {
+            console.log(errors);
+        })
     };
 
     return (
